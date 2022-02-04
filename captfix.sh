9f488734222811267bf78df67c43260dce58672d
Cameron <72923321+bruno-sardine@users.noreply.github.com>
2022-02-04 09:51:58 -0500

    Create captfix.sh
    After fixing the karaoke-style YouTube captions with nimatrueway's subtitle-overlap-fixer.go, use this bash script to fix most of the other problems.  You'll still need to do some manual editing, but it should not be down to things like punctuation and proper names.

---
+++ captfix.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Usage:
+# ./captfix.sh file.srt
+# 
+# Prerequisite:
+# This is for MacOS because the edit in-place syntax of sed is specific for a Mac.
+# For Linux, remove the '' after -i in the sed command.
+# 
+# The problem:
+# Youtube's captions are pretty good, but needs some tuning.
+# After fixing the karaoke-style captions with nimatrueway's subtitle-overlap-fixer.go,
+# the following problems still exist:
+# 
+# 1. First letter of each line is lower case. 
+#    This will fix about 80% of youtube's captions since many lines are the beginning of a sentence.
+# 2. All letter "i" are lower case, where they are not supposed to be:
+#    "winning" > GOOD
+#    "hello i need this" > BAD
+#    "hello, i'm new" > BAD
+# 
+# How it works"
+# 1. The awk command capitalizes the first letter of each line and ignores non-alpha characters.
+#    -What's the deal with the tmp file in awk?  It's the exact same thing as sed -i does, just sed hides it.
+# 2. The sed command fixes all of the lowercase "i" in youtube's captions:
+#    -Example: THIS>> [When i'm sad, i cry in my room] BECOMES>> [When I'm sad I cry in my room]
+#
+
+awk ' { $0=toupper(substr($0,1,1))substr($0,2); print } ' $1 > tmp && mv tmp $1
+sed -i '' "s/ i / I /g;s/i'/I'/g" $1
